b147bc9cb00a54b830477a16d7e1952b4a3c06d6
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 17:03:21 +0100

    division_and_remainder

--- division_and_remainder/src/lib.rs
+++ division_and_remainder/src/lib.rs
@@ -1,6 +1,5 @@
 pub fn divide(x: i32, y: i32) -> (i32, i32) {
     let first = x / y;
     let second = x % y;
-    let tup: (i32, i32) = (first, second);
-    return tup;
+    return (first, second);
 }
