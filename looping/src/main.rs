5b4cefe48acb2ac0f09e86e9d7891dcd470af46f
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 12:13:07 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -2,23 +2,24 @@ use std::io;
 
 
 fn main() {
+
+    let mut count = 1;
+
     loop {
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
 
-        let mut count = 0;
+      
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-            
-        println!("{}", guess);
-        count += 1;
 
         if guess.trim() == "The letter e" {
             println!("Number of trials: {}", count);
             break;
         } else {
+            count += 1;
             continue;
         }
 
