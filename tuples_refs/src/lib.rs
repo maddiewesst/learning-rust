f21811d108a6c1352f4cf79e96fc894358d1f163
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 17:41:03 +0100

    tuples_refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Student(pub(u32, String, String));
+pub struct Student(pub u32,pub String,pub String));
 
 pub fn id(student: &Student) -> u32 {
     student.0
