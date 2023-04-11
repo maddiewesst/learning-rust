b2bc6e00baaf2e9558792df0b558899cb04d1f65
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 12:25:39 +0100

    groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+    let vec = vec[index];
+    vec.to_string()
+}
