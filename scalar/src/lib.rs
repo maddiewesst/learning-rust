4bf9de2c960433633b4460131a8f86a2b666149f
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 10:52:35 +0100

    scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    a + b
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
+}
+
+pub fn quo(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+pub fn rem(a: i32, b: i32) -> i32 {
+    a % b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = rem(5, 2);
+        assert_eq!(result, 1);
+    }
+}
