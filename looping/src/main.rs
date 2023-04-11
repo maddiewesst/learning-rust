2ee2c0f1005dc01c4c69116f9d1ecb9ff9375647
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-11 11:51:07 +0100

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+
+fn main() {
+    loop {
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+
+        let mut count = 0;
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+            
+        println!("{guess}");
+        count += 1;
+
+        if guess.trim() == "The letter e" {
+            println!("Number of trials: {}", count)
+            break;
+        } else {
+            continue;
+        }
+
+    }
+}
