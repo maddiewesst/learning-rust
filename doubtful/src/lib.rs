ecafdc6277ec737146185273e13fb215f25a1336
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-12 12:07:01 +0100

    doubtful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn doubtful(s: String ) {
+    s.push_str("!"):
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
