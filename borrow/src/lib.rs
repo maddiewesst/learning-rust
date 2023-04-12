2e9bfe6e22c2bd22331d76731a792a806244b984
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-12 12:00:20 +0100

    borrow

---
+++ borrow/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn str_len(s: &str) -> usize {
+    s.len
+}
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
