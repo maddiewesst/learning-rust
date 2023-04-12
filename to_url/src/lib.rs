2c4294c76efc5cd40fd07150e4b19c20126a5bdd
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-12 12:46:02 +0100

    to_url

---
+++ to_url/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn to_url(s: &str) -> String {
+    s.to_string().replace(" ", "%20")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
