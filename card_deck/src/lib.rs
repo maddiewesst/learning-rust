3482011a6ed44fa385516abea89d6e021d091e87
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-13 21:15:08 +0100

    card_deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -2,22 +2,22 @@ use rand::Rng;
 
 #[derive(Debug)]
 pub enum Suit {
-    Heart, 
-    Diamond, 
+    Heart,
+    Diamond,
     Spade,
     Club,
 }
 #[derive(Debug, PartialEq)]
 pub enum Rank {
-    Ace, 
-    King, 
+    Ace,
+    King,
     Queen,
     Jack,
     Number(u8),
 }
 
 impl Suit {
-	pub fn random() -> Suit {
+    pub fn random() -> Suit {
         let mut rng = rand::thread_rng();
         // let x = rng.gen();
         // let rand_suit = rng.gen::<>();
@@ -28,22 +28,20 @@ impl Suit {
             4 => return Suit::Club,
             _ => (),
         }
-        
-	}
+    }
 
-	pub fn translate(value: u8) -> Suit {
+    pub fn translate(value: u8) -> Suit {
         match value {
             1 => Suit::Heart,
             2 => Suit::Diamond,
             3 => Suit::Spade,
             4 => Suit::Club,
-            _ => (),
         }
-	}
+    }
 }
 
 impl Rank {
-	pub fn random() -> Rank {
+    pub fn random() -> Rank {
         let mut rng = rand::thread_rng();
         match rng.gen_range(1, 5) {
             1 => return Rank::Ace,
@@ -51,37 +49,31 @@ impl Rank {
             3 => return Rank::Queen,
             4 => return Rank::Jack,
             5 => return Rank::Number(rng.gen_range(2, 10)),
-            _ => ()
         }
-        
-	}
+    }
+}
 
-	}
-
-	pub fn translate(value: u8) -> Rank {
-        match value {
-            1 => return Rank::Ace,
-            13 => return Rank::King,
-            12 => return Rank::Queen,
-            11 => return Rank::Jack,
-            other => return Rank::Number(other)
-	}
+pub fn translate(value: u8) -> Rank {
+    match value {
+        1 => return Rank::Ace,
+        13 => return Rank::King,
+        12 => return Rank::Queen,
+        11 => return Rank::Jack,
+        other => return Rank::Number(other),
+    }
 }
 #[derive(Debug)]
 pub struct Card {
-	pub suit: Suit,
-	pub rank: Rank,
+    pub suit: Suit,
+    pub rank: Rank,
 }
 
 pub fn winner_card(card: &Card) -> bool {
-
     match card.suit {
-        Suit::Spade => {
-            match card.rank {
-                Rank::Ace => return true,
-                _ => false
-        }
-    }
+        Suit::Spade => match card.rank {
+            Rank::Ace => return true,
+            _ => false,
+        },
         other => return false,
     }
 }
