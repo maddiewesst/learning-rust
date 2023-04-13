7593d14034aa8267fbd8bf6e1de4a3906c2a5eda
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-13 21:02:01 +0100

    card_deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,83 @@
+
+#[derive(Debug)]
+pub enum Suit {
+    Heart, 
+    Diamond, 
+    Spade,
+    Club,
+}
+#[derive(Debug, PartialEq)]
+pub enum Rank {
+    Ace, 
+    King, 
+    Queen,
+    Jack,
+    Number(u8),
+}
+
+impl Suit {
+	pub fn random() -> Suit {
+        let mut rng = rand::thread_rng();
+        // let x = rng.gen();
+        // let rand_suit = rng.gen::<>();
+        match rng.gen_range(1, 4) {
+            1 => return Suit::Heart,
+            2 => return Suit::Diamond,
+            3 => return Suit::Spade,
+            4 => return Suit::Club,
+        }
+        
+	}
+
+	pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+        }
+	}
+}
+
+impl Rank {
+	pub fn random() -> Rank {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(1, 5) {
+            1 => return Rank::Ace,
+            2 => return Rank::King,
+            3 => return Rank::Queen,
+            4 => return Rank::Jack,
+            5 => return Rank::Number(rng.gen_range(2, 10)),
+        }
+        
+	}
+
+	}
+
+	pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => return Rank::Ace,
+            13 => return Rank::King,
+            12 => return Rank::Queen,
+            11 => return Rank::Jack,
+            other => return Rank::Number(value)
+	}
+}
+#[derive(Debug)]
+pub struct Card {
+	pub suit: Suit,
+	pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+
+    match card.suit {
+        Suit::Spade => {
+            match card.rank {
+                Rank::Ace => return true,
+                _ => false
+        }
+    }
+        other => return false,
+    }
+}
