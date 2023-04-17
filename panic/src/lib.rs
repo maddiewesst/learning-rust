354f899e88e8d32bb5677775d01d6cd0aaa5ce8d
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-17 15:36:53 +0100

    panic

---
+++ panic/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn open_file(s: &str) -> File {
+    File::open(s).unwrap()
+}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
