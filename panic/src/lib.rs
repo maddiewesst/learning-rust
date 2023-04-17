b9fbc0fdd1bce213e9939c7facd00080a098ede9
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-17 15:57:27 +0100

    panic

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,14 +1,8 @@
 use std::fs::File;
 
 pub fn open_file(s: &str) -> File {
-    let file_result = File::open(s);
-
-    let file = match file_result {
-        Ok(file) => file,
-        Err(error) => panic!("File not found"),
-    }
+    return File::open(s).expect("File not found");
 }
-
 
 
 
