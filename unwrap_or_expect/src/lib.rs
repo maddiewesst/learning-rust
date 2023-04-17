e1348dd5f5c2a0404450370ee571ba6ae49bfdf8
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-17 15:27:20 +0100

    unwrap_or_expect

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,19 @@
+pub enum Security {
+	Unknown,
+	High,
+	Medium,
+	Low,
+	BlockServer,
+}
+
+pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
+
+	match security_level { 
+		Security::Unknown => server.unwrap(),
+		Security::High => server.expect("ERROR: program stops"),
+		Security::Medium => server.unwrap_or("WARNING: check the server".to_string()),
+		Security::Low => server.unwrap_or_else(|val| format!("Not found: {}", val).to_string()),
+		Security::BlockServer => server.unwrap_err(),
+	}
+}
+
