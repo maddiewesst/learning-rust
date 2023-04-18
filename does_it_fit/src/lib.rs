b3cab2fb211ea1386de2ef657cdbb75e2e44de79
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-18 20:27:30 +0100

    does_it_fit

---
+++ does_it_fit/src/lib.rs
@@ -0,0 +1,42 @@
+pub fn area_fit(
+	x: usize,
+	y: usize,
+	objects: areas_volumes::GeometricalShapes,
+	times: usize,
+	a: usize,
+	b: usize,
+) -> bool {
+    let object_area = match objects {
+        GeometricalShapes::Square => square_area(a),
+        GeometricalShapes::Circle => circle_area(a) as usize,
+        GeometricalShapes::Rectangle => rectangle_area(a, b),
+        GeometricalShapes::Triangle => triangle_area(a, b) as usize,
+    };
+    let area = x * y;
+    let max_objects = area / object_area;
+    max_objects >= times
+}
+pub fn volume_fit(
+	x: usize,
+	y: usize,
+	z: usize,
+	objects: areas_volumes::GeometricalVolumes,
+	times: usize,
+	a: usize,
+	b: usize,
+	c: usize,
+) -> bool {
+    match objects {
+        let object_volume = match objects {
+            GeometricalVolumes::Cube => cube_volume(a),
+            GeometricalVolumes::Sphere => sphere_volume(a) as usize,
+            GeometricalVolumes::Cone => cone_volume(a, c) as usize,
+            GeometricalVolumes::Pyramid => triangular_pyramid_volume(b as f64, a) as usize,
+            GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c),
+        };
+        let volume = x * y * z;
+        let max_objects = volume / object_volume;
+        max_objects >= times
+    }
+
+}
