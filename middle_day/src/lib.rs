15cedd0eb020e8d9d937a04d16ce523e5862b086
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-18 17:43:02 +0100

    middle_day

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,12 @@
+
+use chrono::{Datelike, NaiveDate, Weekday};
+
+pub fn middle_day(year: i32) -> Option<Weekday> {
+    let is_leap_year = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
+    let days_in_year = if is_leap_year { 366 } else { 365 };
+    if days_in_year % 2 == 0 {
+        return None;
+    }
+    let middle_day = NaiveDate::from_ymd_opt(year, 1, 1)?.num_days_from_ce() + days_in_year / 2;
+    NaiveDate::from_num_days_from_ce_opt(middle_day as i32).map(|date| date.weekday())
+}
