dd6d3de691d46b49661744e34e2411e67d0ed618
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-19 00:03:50 +0100

    boxing_todo

---
+++ boxing_todo/src/err.rs
@@ -0,0 +1,45 @@
+use std::fmt;
+use std::fmt::Display;
+use std::error::Error;
+
+
+#[derive(Debug)]
+pub enum ParseErr {
+    Empty,
+    Malformed(Box<dyn Error>)
+}
+
+// required by error trait
+impl Display for ParseErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Fail to parse todo")
+    }
+}
+
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseErr::Empty => None,
+            ParseErr::Malformed(_) => Some(self),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ReadErr {
+    pub child_err: Box<dyn Error>
+}
+
+// required by error trait
+impl Display for ReadErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Fail to read todo file")
+    }
+}
+
+impl Error for ReadErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        return Some(&*self.child_err);
+    }
+}
+
---
+++ boxing_todo/src/lib.rs
@@ -0,0 +1,34 @@
+mod err;
+use err::{ ParseErr, ReadErr };
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Read;
+pub use std::error::Error;
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Task {
+    pub id: u32,
+    pub description: String,
+    pub level: u32,
+}
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct TodoList {
+    pub title: String,
+    pub tasks: Vec<Task>,
+}
+
+impl TodoList {
+    pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
+        let mut file = File::open(path)?;
+        let mut content: String = String::new();
+        file.read_to_string(&mut content)?;
+        let json_value: TodoList = serde_json::from_str(&content)?;
+
+        if json_value.tasks.is_empty(){
+            return Err(Box::new(ParseErr::Empty));
+        }else{
+            Ok(json_value)
+        }
+    }
+}
