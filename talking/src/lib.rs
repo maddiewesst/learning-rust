c7dc70894c54bb0681755e87e751bf9b1fe8cc61
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-20 14:05:15 +0100

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn talking(text: &str) -> &str {
-    if text.is_empty() {
+    if text.is_empty() || text.chars().all(char::is_whitespace) {
         "Just say something!"
     } else if text.to_uppercase() == text && text.ends_with('?') {
         "Quiet, I am thinking!"
-    } else if text.to_uppercase() == text {
+    } else if text.to_uppercase() == text && text.ends_with('!') {
         "There is no need to yell, calm down!"
     } else if text.ends_with('?') {
         "Sure."
