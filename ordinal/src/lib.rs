8a1a7a9d1685ffeccab3fa75f6813d74da0bd24b
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-20 11:37:21 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -2,9 +2,9 @@ pub fn num_to_ordinal(x: u32) -> String {
     let last = x % 10;
 
     match last {
-        1 => return x.to_string() + "st",
-        2 => return x.to_string() + "nd",
-        3 => return x.to_string() + "rd",
+        1 if x != 11 => return x.to_string() + "st",
+        2 if x != 12 => return x.to_string() + "nd",
+        3 if x != 13 => return x.to_string() + "rd",
         _ => return x.to_string() + "th",
     }
 }
