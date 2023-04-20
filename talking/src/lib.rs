1d0aa3998656c95e9debe1929975f6795db9e7e6
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-20 16:18:16 +0100

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,10 +1,22 @@
 pub fn talking(text: &str) -> &str {
+    
     let trimmed = text.trim();
+    let mut caps = false;
+
+    for c in text.chars().filter(|c| c.is_alphabetic()) {
+        if !c.is_uppercase() {
+            caps = false;
+            break;
+        } else {
+        caps = true;
+        }
+    }
+
     if trimmed.is_empty() {
         "Just say something!"
-    } else if trimmed.to_uppercase() == trimmed && text.ends_with('?') {
+    } else if caps && text.ends_with('?') {
         "Quiet, I am thinking!"
-    } else if trimmed.to_uppercase() == trimmed {
+    } else if caps {
         "There is no need to yell, calm down!"
     } else if text.ends_with('?') {
         "Sure."
