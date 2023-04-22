3a1fa3217794980265bf13fe77ee814e881c79b7
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-22 08:18:54 +0100

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -3,18 +3,18 @@ pub fn scytale_cipher(message: String, i: u32) -> String {
         
     // }
 	let len = message.len();
-    let cols = (len as f32 / i as f32).ceil() as usize;
-    let mut result = Vec::new();
+    let rows = (len as f32 / i as f32).ceil() as usize;
+    let mut result= vec![' '; rows * i as usize];
 
-    if len % 2 == 0 {
-        result = vec![' '; len];
-    } else {
-        result = vec![' '; len + 1];
+    // if len % 2 == 0 {
+    //     result = vec![' '; len];
+    // } else {
+    //     result = vec![' '; len + 1];
 
-    } 
+    // } 
 
-for col in 0..cols {
-    let start_index = col * i as usize;
+for row in 0..rows {
+    let start_index = row * i as usize;
 
     let mut chars: Vec<char> = message.chars().skip(start_index).take(i as usize).collect();
     chars.resize(i as usize, ' ');
@@ -22,8 +22,8 @@ for col in 0..cols {
 
 
     for (i, c) in chars.into_iter().enumerate() {
-        let index = i * cols + col;
-        if index >= len { 
+        let index = i * rows + row;
+        if index >= result.len() { 
         continue; 
         } else {
             result[index] = c;
@@ -31,5 +31,6 @@ for col in 0..cols {
     }
 }
 
-result.iter().collect()
+let string: String = result.into_iter().collect();
+string.trim().to_string()
 }
