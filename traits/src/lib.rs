a99cd1cbb73491980683c932ed689b4a49584f51
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-26 22:28:31 +0100

    traits

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -42,7 +42,7 @@ impl Food for Fruit {
 
 impl Food for Meat {
     fn gives(&self) -> f64 {
-        let protein = (self.weight_in_kg - (1.0 - self.fat_content)) * 4.0;
+        let protein = (self.weight_in_kg * (1.0 - self.fat_content)) * 4.0;
         let fat = (self.fat_content * self.weight_in_kg) * 9.0;
         protein + fat
     }
