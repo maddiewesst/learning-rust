0327b773f8fa047765c5e7b143078c11ca65aded
maddiewesst <madeleinewesstrom@gmail.com>
2023-04-26 14:36:36 +0100

    generics_list

---
+++ generics_list/src/lib.rs
@@ -0,0 +1,59 @@
+#[derive(Clone, Debug)]
+pub struct List<T> {
+    pub head: Option<Node<T>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> List<T> {
+        List {
+            head: None
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let list = self.head.take();
+    
+        match list {
+            Some(node) => {
+                let new_node = Node { 
+                    value,
+                    next: Some(Box::new(node)),
+                };
+                self.head = Some(new_node);
+            }
+            None => {
+                let new_node = Node { 
+                    value,
+                    next: None,
+                };
+                self.head = Some(new_node);
+            } 
+
+        }
+        
+          
+
+    }
+    pub fn pop(&mut self) {
+
+        if let Some(node) = self.head.take() {
+            self.head = Some(*node.next.unwrap());
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            count += 1;
+            current = node.next.as_ref().map(|boxed_node| &**boxed_node);
+        }
+        count
+    }
+}
