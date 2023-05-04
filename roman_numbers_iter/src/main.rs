eb8599ba049a9cd7f660c84167af3af8e29abb1c
maddiewesst <madeleinewesstrom@gmail.com>
2023-05-04 16:17:02 +0100

    roman_numbers_iter

--- roman_numbers_iter/src/main.rs
+++ roman_numbers_iter/src/main.rs
@@ -1,7 +1,7 @@
 use roman_numbers_iter::RomanNumber;
 
 fn main() {
-	let mut number = RomanNumber::from(0);
+	let mut number = RomanNumber::from(1);
 
 	println!("{:?}", number);
 	println!("{:?}", number.next());
